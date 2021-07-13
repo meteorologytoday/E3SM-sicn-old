111744e7a12ff9feac5d34851e54612e2a06d320
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2021-07-13 14:50:09 -0700

    Update

--- components/mpas-source/src/core_seaice/core_seaice/build_options.mk
+++ components/mpas-source/src/core_seaice/core_seaice/build_options.mk
@@ -3,7 +3,10 @@ ifeq "$(ROOT_DIR)" ""
 endif
 EXE_NAME=seaice_model
 NAMELIST_SUFFIX=seaice
-FCINCLUDES += -I$(ROOT_DIR)/core_seaice/column -I$(ROOT_DIR)/core_seaice/shared -I$(ROOT_DIR)/core_seaice/analysis_members -I$(ROOT_DIR)/core_seaice/model_forward
+FCINCLUDES += -I$(ROOT_DIR)/core_seaice/column -I$(ROOT_DIR)/core_seaice/shared -I$(ROOT_DIR)/core_seaice/analysis_members -I$(ROOT_DIR)/core_seaice/model_forward 
+# TY modified begin: 2021/07/07
+FCINCLUDES += -I$(ROOT_DIR)/core_seaice/sicn_mod
+# TY modified end 
 override CPPFLAGS += -DCORE_SEAICE
 ifneq "$(ESM)" ""
 override CPPFLAGS += -Dcoupled -DCCSMCOUPLED
